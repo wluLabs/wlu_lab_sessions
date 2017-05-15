c2f1f8c9d4324a4479c1e687e0253c3f81134904
GREG-WS\Greg <mensatech@gmail.com>
2017-05-14 20:34:43 -0400

    update code

---
+++ api/controllers/QueryController.js
@@ -0,0 +1,11 @@
+/**
+ * QueryController
+ *
+ * @description :: Server-side logic for managing queries
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
