3933d1429677dbec0ed87b8ba3e88eef8bc0d58b
GREG-WS\Greg <mensatech@gmail.com>
2017-05-14 20:33:47 -0400

    add to code

--- api/controllers/SessionTypeController.js
+++ api/controllers/SessionTypeController.js
@@ -6,6 +6,17 @@
  */
 
 module.exports = {
+	find: function(req, res){
+		var session_type = req.param("session_type");
+		SessionType.findOne({id: session_type}).exec(function (err, sessiontype) {
+			  if(!sessiontype){
+				  console.log('no session exists'); 
+			  }
+			  if(sessiontype){
+				  res.json(200, sessiontype);
+			  } 
+		});
+	}
 	
 };
 
